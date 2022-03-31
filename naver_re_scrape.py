fd662335c2f0f99660cf52912c0450329da54a12
harryphiles <l.bangsg@gmail.com>
2022-04-01 00:05:25 +0900

    v0.4

--- naver_re_scrape.py
+++ naver_re_scrape.py
@@ -1,17 +1,13 @@
-# v0.3
+# v0.4
 
+from operator import itemgetter
 import requests
 import json
 import logging
 
 url = 'https://m.land.naver.com/complex/getComplexArticleList' #basic url
 
-l_spc2 = []
-l_prc = []
-l_prc_min = []
-l_desc = []
-l_flrInfo = []
-#title = []
+list = []
 
 def get_info(tradTpCd, spc_min, spc_max, hscpNo):
     param = {
@@ -28,7 +24,6 @@ def get_info(tradTpCd, spc_min, spc_max, hscpNo):
 
     logging.basicConfig(level=logging.INFO)
     page = 0
-
 
     while True:
         page += 1
@@ -48,70 +43,60 @@ def get_info(tradTpCd, spc_min, spc_max, hscpNo):
             break
         elif param['page'] == 1:
             print('{}'.format(result['list'][0]['atclNm'])) #, print_url)
-            l_spc2.clear()
-            l_prc.clear()
-            l_prc_min.clear()
-            l_desc.clear()
-            l_flrInfo.clear()
-                # title.clear()
-                # title.append('{}'.format(result['list'][0]['atclNm']))
-
+            list.clear()
+  
         for item in result['list']:
-            spc2 = '{:.4}'.format(item['spc2'])
-            prc = '{:8}'.format(item['prcInfo'])
-            #prc_max = '{:8}'.format(item['sameAddrMaxPrc'])
-            prc_min = '{:8}'.format(item['sameAddrMinPrc'])
-            try:
-                desc = item['atclFetrDesc']
-            except:
-                desc = 'none'
-            if item['flrInfo'][:item['flrInfo'].find('/')].isnumeric():
-                flrInfo = '{:>5}'.format(item['flrInfo'])
-            else:
-                flrInfo = '{:>4}'.format(item['flrInfo'])
             if float(item['spc2']) >= spc_min and float(item['spc2']) < spc_max:
-                l_spc2.append(spc2)
-                l_prc.append(prc)
-                l_prc_min.append(prc_min)
-                l_desc.append(desc)
-                l_flrInfo.append(flrInfo)        
+                spc2 = '{:.4}'.format(item['spc2'])
+                prc = '{:8}'.format(item['prcInfo'])
+                prc_min = '{:8}'.format(item['sameAddrMinPrc'])
+                try:
+                    desc = item['atclFetrDesc']
+                except:
+                    desc = 'none'
+                if item['flrInfo'][:item['flrInfo'].find('/')].isnumeric():
+                    flrInfo = '{:>5}'.format(item['flrInfo'])
+                else:
+                    flrInfo = '{:>4}'.format(item['flrInfo'])
+                x = [spc2, prc, prc_min, flrInfo, desc]
+                list.append(x)     
 
         if result['moreDataYn'] == 'N':
             break
 
-def get_min():
-    for i in sorted(set(l_spc2)):
-        if l_prc[l_spc2.index(i)] != l_prc_min[l_spc2.index(i)]:
-            print('{} | {}'.format(i, l_prc_min[l_spc2.index(i)]))
-        else:
-            print('{} | {}'.format(i, l_prc[l_spc2.index(i)]))
+def get_min(): #--> new function to get min
+    s = sorted(list, key=itemgetter(2))
+    spc = []
+    for i in s:
+        spc.append(i[0])
+    spc_s = sorted(set(spc))
+    for i in spc_s:
+        print(i, s[spc.index(i)][2])
 
-def get_list():
-    for i in range(len(l_spc2)):
-        if l_prc[i] != l_prc_min[i]:
-            print('{} | {} | {} | {} | {}'.format(l_spc2[i], l_prc[i], l_flrInfo[i], l_prc_min[i], l_desc[i]))
+def get_list(): #--> new function to get list
+    for i in list:
+        if i[1] != i[2]:
+            print('{} | {} | {} | {} | {}'.format(i[0], i[1], i[3], i[2], i[4]))
         else:
-            print('{} | {} | {} |           | {}'.format(l_spc2[i], l_prc[i], l_flrInfo[i], l_desc[i]))
+            print('{} | {} | {} |           | {}'.format(i[0], i[1], i[3], i[4]))
 
 ### watch list
 watch_list_1 = [
-    [111059, 40], # 광교지웰홈스
-    [103994, 45], # 에듀하임
+    [111059, 40],  # 광교지웰홈스
+    [109829, 43],  # 광교_힐스테이트광교
+    [103994, 45],  # 에듀하임
     [114777, 50]  # 힐스테이트광교중앙역
 ]
-
 watch_list_2 = [
-    [102530, 40], # 수지_푸르지오월드마크(주상복합)
-    [10244, 40],  # 정자_두산위브파빌리온
+    [102530, 40],  # 수지_푸르지오월드마크(주상복합)
+    [10244, 40],   # 정자_두산위브파빌리온
     [9896, 40]   # 구성_성호샤인힐즈
 ]
-
 watch_list_3 = [
     [114593, 40],  # 광교_sk뷰레이크타워
     [111001, 40],  # 광교_중흥S-클래스
     [116449, 40]  # 광교_포레나광교
 ]
-
 watch_list_4 = [
     [120682, 40],  # 광교_효성해링턴타워
     [120134, 40],  # 광교_효성해링턴타워레이크
@@ -119,19 +104,26 @@ watch_list_4 = [
     [105468, 40]  # 광교_힐스테이트레이크
 ]
 
-### watch list search
-for i in watch_list_1:              # change number to show results of different lists
-    get_info('B1', i[1], 85, i[0])  # 기본
-    get_min()                       # 보기: 각 평수당 최소 보증금만
-    get_list()                      # 보기: 모든 매물
-    # print('\n')
+### watch list search ###
+def search_min(x):
+    for i in x:                         # change number to show results of different lists
+        get_info('B1', i[1], 85, i[0])  # 기본
+        get_min()                       # 보기: 각 평수당 최소 보증금만
 
-### indivisual search
-# get_info('B1', 40, 130, 114777)
-# get_min()
-# get_list()
+def search_list(x):
+    for i in x:                         # change number to show results of different lists
+        get_info('B1', i[1], 85, i[0])  # 기본
+        get_list()                      # 보기: 모든 매물
 
+### indivisual search ###
+def search_individual():
+    get_info('B1', 40, 130, 111059)
+    get_list()
 
+search_min(watch_list_1)
+#search_list(watch_list_1)
+
+'''
 # [120682, 40],  # 광교_효성해링턴타워
 # [120134, 40],  # 광교_효성해링턴타워레이크
 # [105468, 40],  # 광교_힐스테이트레이크
@@ -149,3 +141,4 @@ for i in watch_list_1:              # change number to show results of different
 # [102530, 40],  # 수지_푸르지오월드마크(주상복합)
 # [10244, 40],   # 정자_두산위브파빌리온
 # [9896, 40]   # 구성_성호샤인힐즈
+'''
