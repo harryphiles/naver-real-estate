9b5b93ab05949aec92ff0a58f79127a59e6baf22
harryphiles <l.bangsg@gmail.com>
2022-04-01 00:04:48 +0900

    v0.3

--- naver_re_scrape.py
+++ naver_re_scrape.py
@@ -1,10 +1,17 @@
-### v0.2
+# v0.3
 
 import requests
 import json
 import logging
 
 url = 'https://m.land.naver.com/complex/getComplexArticleList' #basic url
+
+l_spc2 = []
+l_prc = []
+l_prc_min = []
+l_desc = []
+l_flrInfo = []
+#title = []
 
 def get_info(tradTpCd, spc_min, spc_max, hscpNo):
     param = {
@@ -22,6 +29,7 @@ def get_info(tradTpCd, spc_min, spc_max, hscpNo):
     logging.basicConfig(level=logging.INFO)
     page = 0
 
+
     while True:
         page += 1
         param['page'] = page
@@ -31,24 +39,25 @@ def get_info(tradTpCd, spc_min, spc_max, hscpNo):
             logging.error('status code: %d' % r.status_code)
             break
 
+        print_url = f'https://m.land.naver.com/complex/info/{hscpNo}?tradTpCd={tradTpCd}&ptpNo=&bildNo=&articleListYN=Y'
+
         load_json = json.loads(r.text)
         result = load_json['result']
         if result is None:
             logging.error('no result')
             break
         elif param['page'] == 1:
-            print('{}'.format(result['list'][0]['atclNm']))
-
-        # print(r.url)
-        # for item in result['list']:
-        #     #logging.info('%s' % (item['atclNm']))
-        #     if float(item['spc2']) < 49 or float(item['spc2']) > 85:
-        #         continue
-        #     #logging.info('%s %s %s' % (item['spc2'], item['prcInfo'], item['flrInfo'])) # item['atclFetrDesc']
-        #     logging.info('{:.4}m2 {:8} {}'.format(item['spc2'], item['prcInfo'], item['flrInfo']))
+            print('{}'.format(result['list'][0]['atclNm'])) #, print_url)
+            l_spc2.clear()
+            l_prc.clear()
+            l_prc_min.clear()
+            l_desc.clear()
+            l_flrInfo.clear()
+                # title.clear()
+                # title.append('{}'.format(result['list'][0]['atclNm']))
 
         for item in result['list']:
-            spc2 = '{:.4}m2'.format(item['spc2'])
+            spc2 = '{:.4}'.format(item['spc2'])
             prc = '{:8}'.format(item['prcInfo'])
             #prc_max = '{:8}'.format(item['sameAddrMaxPrc'])
             prc_min = '{:8}'.format(item['sameAddrMinPrc'])
@@ -61,38 +70,82 @@ def get_info(tradTpCd, spc_min, spc_max, hscpNo):
             else:
                 flrInfo = '{:>4}'.format(item['flrInfo'])
             if float(item['spc2']) >= spc_min and float(item['spc2']) < spc_max:
-                if prc != prc_min:
-                    print('{} | {} | {} | {} | {}'.format(spc2, prc, flrInfo, prc_min, desc))
-                else:
-                    print('{} | {} | {} |           | {}'.format(spc2, prc, flrInfo, desc))
+                l_spc2.append(spc2)
+                l_prc.append(prc)
+                l_prc_min.append(prc_min)
+                l_desc.append(desc)
+                l_flrInfo.append(flrInfo)        
 
         if result['moreDataYn'] == 'N':
             break
 
+def get_min():
+    for i in sorted(set(l_spc2)):
+        if l_prc[l_spc2.index(i)] != l_prc_min[l_spc2.index(i)]:
+            print('{} | {}'.format(i, l_prc_min[l_spc2.index(i)]))
+        else:
+            print('{} | {}'.format(i, l_prc[l_spc2.index(i)]))
+
+def get_list():
+    for i in range(len(l_spc2)):
+        if l_prc[i] != l_prc_min[i]:
+            print('{} | {} | {} | {} | {}'.format(l_spc2[i], l_prc[i], l_flrInfo[i], l_prc_min[i], l_desc[i]))
+        else:
+            print('{} | {} | {} |           | {}'.format(l_spc2[i], l_prc[i], l_flrInfo[i], l_desc[i]))
+
+### watch list
+watch_list_1 = [
+    [111059, 40], # 광교지웰홈스
+    [103994, 45], # 에듀하임
+    [114777, 50]  # 힐스테이트광교중앙역
+]
+
+watch_list_2 = [
+    [102530, 40], # 수지_푸르지오월드마크(주상복합)
+    [10244, 40],  # 정자_두산위브파빌리온
+    [9896, 40]   # 구성_성호샤인힐즈
+]
+
+watch_list_3 = [
+    [114593, 40],  # 광교_sk뷰레이크타워
+    [111001, 40],  # 광교_중흥S-클래스
+    [116449, 40]  # 광교_포레나광교
+]
+
+watch_list_4 = [
+    [120682, 40],  # 광교_효성해링턴타워
+    [120134, 40],  # 광교_효성해링턴타워레이크
+    [107999, 43],  # 광교_더샵레이크파크
+    [105468, 40]  # 광교_힐스테이트레이크
+]
+
 ### watch list search
-# watch_list_primary = [
-#     111059, # 광교지웰홈스
-#     103994, # 에듀하임
-#     114777  # 힐스테이트광교중앙역
-# ]
+for i in watch_list_1:              # change number to show results of different lists
+    get_info('B1', i[1], 85, i[0])  # 기본
+    get_min()                       # 보기: 각 평수당 최소 보증금만
+    get_list()                      # 보기: 모든 매물
+    # print('\n')
 
-# for i in watch_list_primary:
-#     get_info('B1', i)
+### indivisual search
+# get_info('B1', 40, 130, 114777)
+# get_min()
+# get_list()
 
-## indivisual search
-get_info('B1', 40, 85, 111001)
 
-# 120134 # 광교효성해링턴타워레이크
-# 120682 # 광교효성해링턴타워
-# 105468 # 광교힐스테이트레이크
-# 107999 # 광교더샵레이크파크
-# 109829 # 힐스테이트광교
-# 114777 # 힐스테이트광교중앙역
-# 103994 # 에듀하임
-# 114593 # 광교sk뷰레이크타워
-# 111001 # 중흥S-클래스
-# 116449 # 포레나광교
-# 110643 # 광교아이파크
-# 120316 # 더샵광교레이크시티
-# 110644 # 광교더샵
-# 111059 # 광교지웰홈스
+# [120682, 40],  # 광교_효성해링턴타워
+# [120134, 40],  # 광교_효성해링턴타워레이크
+# [105468, 40],  # 광교_힐스테이트레이크
+# [107999, 43],  # 광교_더샵레이크파크
+# [109829, 43],  # 광교_힐스테이트광교
+# [114777, 50],  # 힐스테이트광교중앙역
+# [103994, 50],  # 에듀하임
+# [114593, 40],  # 광교_sk뷰레이크타워
+# [111001, 40],  # 광교_중흥S-클래스
+# [116449, 40],  # 광교_포레나광교
+# [110643, 40],  # 광교(남)_아이파크
+# [120316, 40],  # 광교(남)_더샵광교레이크시티
+# [110644, 40],  # 광교(남)_더샵
+# [111059, 40],  # 광교지웰홈스
+# [102530, 40],  # 수지_푸르지오월드마크(주상복합)
+# [10244, 40],   # 정자_두산위브파빌리온
+# [9896, 40]   # 구성_성호샤인힐즈
