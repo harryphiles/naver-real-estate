490eb9da9ed1c3445188f716226a780d0156ed6e
testing <l.bangsg@gmail.com>
2022-04-13 17:31:49 +0900

    changes

--- naver_re_scrape.py
+++ naver_re_scrape.py
@@ -7,3 +7,7 @@ alert(watch_list_a, prc_max, 'data_1')
 time.sleep(random.uniform(61, 120))
 
 alert(watch_list_b, prc_max, 'data_2')
+
+time.sleep(random.uniform(61, 120))
+
+alert(watch_list_c, prc_max, 'data_3')
