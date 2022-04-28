8bba904ba5d7ebf93ecb0d8b6aa8c45df0f200f3
testing <l.bangsg@gmail.com>
2022-04-28 09:10:33 +0900

    time sleep changes

--- naver_re_scrape.py
+++ naver_re_scrape.py
@@ -10,6 +10,6 @@ time.sleep(random.uniform(61, 120))
 
 alert(watch_list_b, prc_max, 'data_2')
 
-time.sleep(random.uniform(61, 120))
+time.sleep(random.uniform(150, 300))
 
 alert(watch_list_c, 29990, 'data_3')
