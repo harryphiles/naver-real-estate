d70b03a34c16d21170d760a848ea537817cd3385
Sanggyun Bang <58872165+harryphiles@users.noreply.github.com>
2023-04-10 00:26:46 +0900

    apply if name equals main convention on the main script

--- naver_re_scrape.py
+++ naver_re_scrape.py
@@ -1,15 +1,19 @@
 from functions import *
 
-time_check_and_delete()
+def main():
+    time_check_and_delete()
 
-prc_max = 32000
+    prc_max = 32000
 
-alert(watch_list_a, prc_max, 'data_1')
+    alert(watch_list_a, prc_max, 'data_1')
 
-time.sleep(random.uniform(61, 120))
+    time.sleep(random.uniform(61, 120))
 
-alert(watch_list_b, prc_max, 'data_2')
+    alert(watch_list_b, prc_max, 'data_2')
 
-time.sleep(random.uniform(150, 300))
+    time.sleep(random.uniform(150, 300))
 
-alert(watch_list_c, 29990, 'data_3')
+    alert(watch_list_c, 29990, 'data_3')
+
+if __name__ == "__main__":
+    main()
