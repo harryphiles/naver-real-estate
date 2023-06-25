62a2976e11143d768963d68fe0622586161e5569
Sanggyun Bang <58872165+harryphiles@users.noreply.github.com>
2023-06-25 11:28:15 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -33,8 +33,6 @@ def get_info_new(tradTpCd, spc_min, spc_max, hscpNo):
     }
 
     header = {
-        #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
-        #'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 6P Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36',
         'Referer': 'https://m.land.naver.com/'
     }
